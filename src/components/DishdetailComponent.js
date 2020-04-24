687b35f6362c1b7efad626e5255254ac5d9231b3
Shahjahan Md. Swajan <swajan.mis13.du@gmail.com>
2020-04-24 17:24:32 +0600

    Front End With React week 3 assignment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
 	Card,
 	CardImg,
@@ -7,8 +7,130 @@ import {
 	CardText,
 	Breadcrumb,
 	BreadcrumbItem,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	Row,
+	Label,
+	Col,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { LocalForm, Control, Errors } from "react-redux-form";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => !val || val.length >= len;
+
+class CommentFormComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isModalOpen: false,
+		};
+
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	}
+	handleSubmit(values) {
+		alert("Submitted your comment as: " + JSON.stringify(values));
+	}
+	render() {
+		return (
+			<div>
+				<Button outline onClick={this.toggleModal}>
+					<span className="fa fa-pencil"></span> Submit Comment
+				</Button>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+							<Row className="form-group">
+								<Label className="col-12" htmlFor="rating">
+									Rating
+								</Label>
+								<Col>
+									<Control.select
+										model=".rating"
+										name="rating"
+										id="rating"
+										className="form-control"
+										validators={{ required }}
+									>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+									<Errors
+										className="text-danger"
+										model=".rating"
+										show="touched"
+										messages={{
+											required: "Required",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label className="col-12" htmlFor="name">
+									Your Name
+								</Label>
+								<Col>
+									<Control.text
+										model=".name"
+										id="name"
+										name="name"
+										placeholder="You Name"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(3),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".name"
+										show="touched"
+										messages={{
+											required: "Required",
+											minLength: "Must be greater than 2 characters",
+											maxLength: "Must be 15 characters or less",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="comment" className="col-12">Comment</Label>
+								<Col>
+								<Control.textarea model=".comment" id="comment" name="comment" rows='6' className="form-control" validators={{required}} />
+								<Errors className="text-danger"
+								model=".comment" show='touched' messages={{
+									required: "Required"
+								}}/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+                                <Col>
+                                    <Button onClick={this.toggleModal} type="submit" color="primary">
+                                    Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</div>
+		);
+	}
+}
 
 const RenderDish = ({ dish }) => {
 	if (dish != null) {
@@ -55,6 +177,7 @@ const RenderComments = ({ comments }) => {
 			<div className="col-12 col-md-5 m-1">
 				<h4>Comments</h4>
 				<ul className="list-unstyled">{list}</ul>
+				<CommentFormComponent />
 			</div>
 		);
 	} else {
